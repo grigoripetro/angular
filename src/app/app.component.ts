9944228ed431712d5b8e41272afbffeaf705b0f9
Software Developer <i4uprovider@gmail.com>
2018-07-10 16:24:51 +0300

    updated template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}} This is example</h1>`,
+  template: `<li *ngFor="let hero of heroes"></li>`,
 })
 export class AppComponent  { name = 'Angular'; }
